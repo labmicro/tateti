9351cdbf5fcbec68313814ed901e6239ac0f1bd4
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2022-03-30 23:00:27 -0300

    Se agrega una tercera prueba que falla

--- test/tateti.js
+++ test/tateti.js
@@ -20,11 +20,21 @@ chai.use(chaiHttp);
 // - Cuando no quedan casillas vacias y no hay un ganador entonces hay un empate.
 
 describe("Juego de TaTeTi", () => {    
+    let juego = {
+        jugadores: ['Juan', 'Pedro']
+    }
+    let movimientos = [
+        { jugador: 'Juan', columna: 0, fila: 0 },
+        { jugador: 'Pedro', columna: 1, fila: 0 },
+        { jugador: 'Juan', columna: 0, fila: 1 },
+        { jugador: 'Pedro', columna: 1, fila: 1 },
+        { jugador: 'Juan', columna: 0, fila: 2 },
+    ]
     describe("Se empieza un juego nuevo", () => {
         it("Todos los casilleros estan vacios y le toca mover al primer jugador", (done) => {
             chai.request(server)
             .put("/empezar")
-            .send({ jugadores: ['Juan', 'Pedro'] })
+            .send(juego)
             .end((err, res) => {
                 res.should.have.status(200);
                 res.should.to.be.json;
@@ -43,14 +53,10 @@ describe("Juego de TaTeTi", () => {
     });
     describe("El primer jugador hace su primer movimiento", () => {
         it("El casillero queda ocupado y le toca al otro jugador", (done) => {
-            chai.request(server)
-                .put("/empezar")
-                .send({ jugadores: ['Juan', 'Pedro'] })
-                .end();
-
+            chai.request(server).put("/empezar").send(juego).end();
             chai.request(server)
                 .put("/movimiento")
-                .send({ jugador: 'Juan', columna: 0, fila: 0 })
+                .send(movimientos[0])
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.should.to.be.json;
@@ -62,7 +68,28 @@ describe("Juego de TaTeTi", () => {
                         [' ', ' ', ' '],
                     ]);
                     done()
+                })
+        });
+    });
+    describe("El segundo jugador hace su primer movimiento", () => {
+        it("El casillero queda ocupado y le toca al otro jugador", (done) => {
+            chai.request(server).put("/empezar").send(juego).end();
+            chai.request(server).put("/movimiento").send(movimientos[0]).end();
+            chai.request(server)
+                .put("/movimiento")
+                .send(movimientos[1])
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.to.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('turno').eql('Juan');
+                    res.body.should.have.property('tablero').eql([
+                        ['x', 'o', ' '],
+                        [' ', ' ', ' '],
+                        [' ', ' ', ' '],
+                    ]);
+                    done()
                 });
         });
-    });    
+    });
 });
