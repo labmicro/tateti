a5dd541f11830c7d145a2e056939fe6ca9dd72e3
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2022-03-30 23:00:53 -0300

    Implementacion minima de los turnos de juego

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+var jugadores;
+var tablero;
+var turno;
+
+const marcas = ['x', 'o'];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.put('/empezar', function (request, response) {
+  turno = 0;
+  jugadores = request.body.jugadores;
+  tablero = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+  ];
+
   response.setHeader('Content-Type', 'application/json');  
-  response.send({turno: 'Juan', tablero: [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-  ]});
+  response.send({turno: jugadores[turno], tablero: tablero});
 });
 
 router.put('/movimiento', function (request, response) {
+  const columna = request.body.columna;
+  const fila = request.body.fila;
+
+  tablero[fila][columna] = marcas[turno];
+  turno = (turno + 1) % 2;
+
   response.setHeader('Content-Type', 'application/json');  
-  response.send({turno: 'Pedro', tablero: [
-    ['x', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-  ]});
+  response.send({turno: jugadores[turno], tablero: tablero});
 });
 
 module.exports = router;
