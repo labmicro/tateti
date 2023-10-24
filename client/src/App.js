8e89064e66c86f69e35f52f7ad96d8b96765807f
Mateo-Sanchez14 <mateo-sanchez@hotmail.com>
2023-10-24 15:42:32 -0300

    add initial frontend

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+import TicTacToe from "./TicTacToe";
+const App = () => {
+  return (
+    <div className="App">
+      <TicTacToe />
+    </div>
+  );
+};
+export default App;
